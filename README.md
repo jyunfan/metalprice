ea0dd73f32fd49910bc0c1ba0eba429bbf0a331a
Jyun-Fan Tsai <jyunfan@gmail.com>
2012-06-10 21:43:50 +0800

    Collect historical data from 1996 to 2011.

--- README.md
+++ README.md
@@ -3,13 +3,15 @@ metalprice
 
 Looking for historical prices of metal trading? 
 
-1. If you need only hitorical data before 2012, just use data/LondonFix.csv
-The data are collected from www.kitco.com, which provides daily prices of gold, silver, platinum, and palladium.
+Here is hitorical data before 2012. Open data/LondonFix.csv to get what you need.
+The data are collected from www.kitco.com, which provides daily prices of gold,
+silver, platinum, and palladium.
 
 Format of the csv file:
 Date,Gold:AM,Gold:PM,Silver,Platinum:AM,Platinum:PM,Palladium:AM,Palladium:PM
 2011-12-29,1537.50,1531.00,26.1600,1364.00,1354.00,636.00,630.00
 
-2. If you need newest data, run kitco.pm. No installation. You only need
-perl installed on your computer. The program gets newest data and appends it
-to data/LondonFix.csv
+Need newest data?
+We provide a perl program. Just run bin/kitco.pm. You only need perl installed
+on your computer. The program gets newest data and appends it to
+data/LondonFix.csv
--- bin/kitco.pm
+++ bin/kitco.pm
@@ -11,25 +11,56 @@ my $datadir  = File::Spec->catdir(dirname(__FILE__), '..', 'data');
 my $outfile  = File::Spec->catfile($datadir, 'LondonFixMetal.csv');
 
 my $kitco_current_url = "http://www.kitco.com/gold.londonfix.html";
-my $kitco_historical_urls = [
-#    'http://www.kitco.com/londonfix/gold.londonfix05.html',
-    'http://www.kitco.com/londonfix/gold.londonfix06.html',
-    'http://www.kitco.com/londonfix/gold.londonfix07.html',
-    'http://www.kitco.com/londonfix/gold.londonfix08.html',
-    'http://www.kitco.com/londonfix/gold.londonfix09.html',
-    'http://www.kitco.com/londonfix/gold.londonfix10.html',
-    'http://www.kitco.com/londonfix/gold.londonfix11.html'
-];
+my $kitco_historical_url_prefix =
+    'http://www.kitco.com/londonfix/gold.londonfix';
 
-my $date_pat = qr/(\d{4}-\d{2}-\d{2}|\w+\s+\d+,\s+\d{4})/;
-my $price_pat = qr#<p>([\d.]+|-)</p>#;
+my $date_pat = qr/((?:\d{4}-\d{2}-\d{2})|(?:\w+\s+\d+,\s*\d{4}))/;
+my $price_pat = qr#<(?:p|b)>([\d\w\.\-\s\,<>\\]*)</(?:p|b)>#i;
 my $header = "Date,Gold:AM,Gold:PM,Silver,Platinum:AM,Platinum:PM,Palladium:AM,Palladium:PM";
+my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 
-#-----
+#----------
+# example: 1996-2005
+# Multi-line
+#    <tr ALIGN="CENTER"> 
+#      <TD bgColor=#f3f3e4 COLSTART="1"><b>December 24, 1997</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="2"><b>296.10</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="3"><b>Closed</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="4"><b>6.2675</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="5"><b>359.00</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="6"><b>Closed</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="7"><b>191.00</b></td>
+#      <TD bgColor=#f3f3e4 COLSTART="8"><b>Closed</b></td>
+#    </tr>
+#
+# example: 2006-
+# Single line
+# <tr align=middle><td height=24 bgcolor=#f3f3e4><p>2011-12-29</p></td><td height=24 bgcolor=#e1e1bd><p>1537.50</p></td><td height=24 bgcolor=#cccc99><p>1531.00</p></td><td height=24 bgcolor=#e2e2e2><p>26.1600</p></td><td height=24 bgcolor=#e1e1bd><p>1364.00</p></td><td height=24 bgcolor=#cccc99><p>1354.00</p></td><td height=24 bgcolor=#e1e1bd><p>636.00</p></td><td height=24 bgcolor=#cccc99><p>630.00</p></td></tr>
+
+#----------
+# Input: Format as Aug 25, 2002
+# Output: 2002-08-25
+#----------
+sub normalize_date {
+    my $date = shift;
+    my ($day, $month, $year);
+    $date =~ /(\w+)\s+(\d+),\s*(\d+)/;
+    ($month, $day, $year) = ($1, $2, $3);
+
+    for (my $m=0; $m<12; $m++) {
+        if ($month =~ $months[$m]) {
+            $month = $m+1;
+            last;
+        }
+    } 
+    return sprintf("%s-%02d-%02d", $year, $month, $day);
+}
+
+#----------
 # Get content of urls and save them into files.
 # Input: urls
 # Output: Filenames
-#-----
+#----------
 sub get_save_urls {
     my $urllist = shift;
     my $files = [];
@@ -48,7 +79,7 @@ sub get_save_urls {
 }
 
 #-----
-# Input: (input filename, output filename, a string condition)
+# Input: (input filename, output filename, newer than the date)
 #   $newer_than is a optional condition. Only records newer than this date are stored.
 #   The parameter is useful for incremental updating.
 #-----
@@ -60,16 +91,29 @@ sub get_price {
     # Save result to @d so that we can reverse order
     my @d;
 
-    while(<$ifh>) {
-        my $line = $_;
+    # Keep only data within TR tag
+    chomp(my @lines = <$ifh>);
+    my $content = join('', @lines);
+    close($ifh);
+    my @rows = $content =~ m/<tr.*?<\/tr>/ig;
+
+    for my $line (@rows) {
         next if $line !~ $date_pat;
         my $date = $1;
+        # Get prices
         my @p = $line =~ m/$price_pat/g;
-        @p = map {$_ =~ s/\-//; $_} @p;     # Remove '-' in prices
+        # First element is date.
+        shift @p;
+        # Skip partial result
+        next if ($#p!=6);
+        # Remove non-digital in each price, which means not available
+        @p = map {$_=$_||''; $_ =~ s/[^\d\.]//g; $_} @p;
+        if ($date !~ /^\d/) {
+            $date = normalize_date($date);
+        }
         my $s = "$date," . join(',',@p) . "\n";
         push @d, $s;
     }
-    close($ifh);
 
     if (defined $newer_than) {
         @d = grep {$_ gt $newer_than} @d;
@@ -81,43 +125,63 @@ sub get_price {
 }
 
 sub get_hist_prices {
-    my $urllist = shift;
-    
-    return if (-e $outfile);
-
-    open(my $ofh, '>', $outfile) or die "Cannot write $outfile";
-    print $ofh "$header\n";
-    close($ofh);
+    my ($urllist, $last_rec, $del_ifile) = @_;
 
     for my $url (@{$urllist}) {
         $url =~ /\/([^\/]+)$/;
         my $ifile = File::Spec->catfile($datadir, $1);
         next if !(-e $ifile);
 
-        get_price($ifile, $outfile);
+        get_price($ifile, $outfile, $last_rec);
+
+        unlink($ifile) if defined($del_ifile);
     }
 }
 
-sub get_last_date {
+sub get_last_record {
     my $file = shift;
-    my $last_date;
-    open(my $fh, '<', $file) or die "Cannot open $file";
+    my $last_rec = '1996-01-01';
+    open(my $fh, '<', $file) or return $last_rec;
     while(<$fh>) {
-        $last_date = $_ if $_ =~ /\S+/;
+        $last_rec = $_ if $_ =~ /\S+/;
     }
     close($fh);
-    return $last_date;
+    return $last_rec;
 }
 
 sub main {
-    # Ensure that the HTML is in our disk
-    get_save_urls($kitco_historical_urls);
-    get_hist_prices($kitco_historical_urls); 
+    # Prepare csv header
+    if (! -e $outfile) {
+        open(my $ofh, '>', $outfile) or die "Cannot write $outfile";
+        print $ofh "$header\n";
+        close($ofh);
+    }
 
-    my $last_date = get_last_date($outfile);
-    my $file = get_save_urls([$kitco_current_url])->[0];
-    get_price($file, $outfile, $last_date);
-    unlink($file) if (-e $file); 
+    my @t = localtime(time);
+    my $year_cur = $t[5]+1900;
+    my $last_rec = get_last_record($outfile);
+    my $year_rec = $1;
+    if ($last_rec =~ /([\d]+)/) {
+        $year_rec = $1;
+    } else {
+        $year_rec = 1996;
+        $last_rec = '1996-01-01';
+    }
+
+    for (; $year_rec <= $year_cur; $year_rec++) {
+        my ($url, $del_html); 
+        if ($year_rec < $year_cur) {
+            # Previous years
+            my $short_year = sprintf("%02d", $year_rec>=2000 ? $year_rec-2000 : $year_rec-1900);
+            $url = $kitco_historical_url_prefix . $short_year . '.html'; 
+        } else {
+            # Current year
+            $url = $kitco_current_url;
+            $del_html = 1;
+        }
+        my $file = get_save_urls([$url])->[0];
+        get_hist_prices([$url], $last_rec, $del_html);
+    }
 }
 
 main() unless caller();
